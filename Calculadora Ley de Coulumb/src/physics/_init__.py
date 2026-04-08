ed878a191315c482826d5444232b6a13ab6326da
Andrea Duarte <ajduarteg@uamv.edu.ni>
2026-04-07 19:20:46 -0600

    delete inits

--- Calculadora Ley de Coulumb/src/physics/_init__.py
+++
@@ -1,5 +0,0 @@
-# physics/__init__.py
-"""Funciones físicas: Ley de Coulomb y superposición."""
-from .coulomb import force_between, net_force_on_target
-
-__all__ = ["force_between", "net_force_on_target"]
--- Calculadora Ley de Coulumb/src/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Utilidades auxiliares (validación, etc.)."""
-from .calculator import validate_float
-
-__all__ = ["validate_float"]
